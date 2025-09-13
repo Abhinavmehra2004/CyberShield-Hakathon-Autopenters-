4a5e493e2a4627696c781522d8e8776f8ad8dfc8
Abhinav mehra <102248173+Abhinavmehra2004@users.noreply.github.com>
2025-09-14 01:23:17 +0530

    Update config.py

--- SourceCode/config.py
+++ SourceCode/config.py
@@ -1,9 +1,9 @@
 import os
 
 # Metasploit RPC Configuration
-METASPLOIT_RPC_HOST = os.getenv("METASPLOIT_RPC_HOST", "127.0.0.1")
-METASPLOIT_RPC_PORT = int(os.getenv("METASPLOIT_RPC_PORT", 55552))
-METASPLOIT_RPC_PASS = os.getenv("METASPLOIT_RPC_PASS", "abhinav2004")
+METASPLOIT_RPC_HOST = os.getenv("METASPLOIT_RPC_HOST" )
+METASPLOIT_RPC_PORT = int(os.getenv("METASPLOIT_RPC_PORT" ))
+METASPLOIT_RPC_PASS = os.getenv("METASPLOIT_RPC_PASS" )
 
 # Attack Machine Details
 LHOST = os.getenv("LHOST", "172.25.162.99")
@@ -19,10 +19,10 @@ LINPEAS_PATH = os.getenv("LINPEAS_PATH", "/app/scripts/linpeas.sh")
 WINPEAS_PATH = os.getenv("WINPEAS_PATH", "/app/scripts/winPEASany.exe")
 
 # AI Model Path (LLaMA)
-LLAMA_MODEL_PATH = os.getenv("DEEPSEEK_MODEL_PATH", "/app/models/blobs/sha256-ff82381e2bea77d91c1b824c7afb83f6fb73e9f7de9dda631bcdbca564aa5435")
+LLAMA_MODEL_PATH = os.getenv("LLAMA_MODEL_PATH")
 
 # Haiku API Configuration
 HAIKU_API_KEY = os.getenv("HAIKU_API_KEY")
-HAIKU_API_URL = os.getenv("HAIKU_API_URL", "https://api.haikuscanner.com/v1/scan")
+HAIKU_API_URL = os.getenv("HAIKU_API_URL")
 
 print("all is working")
